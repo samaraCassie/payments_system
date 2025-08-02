8dd88389b9a9f3daab11efb3974cf9bdba56e1bb
samaraCassie <samaraCassie@outlook.com>
2025-08-02 02:22:36 -0300

    fix: ajuste em AutenticacaoController

--- src/main/java/payment_system/usuarios/api/controller/AutenticacaoController.java
+++ src/main/java/payment_system/usuarios/api/controller/AutenticacaoController.java
@@ -1,4 +1,4 @@
-package payment_system.usuarios.api.controller;
+package payment_system.usuarios.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -7,18 +7,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import payment_system.usuarios.application.services.AutorizacaoService;
 import payment_system.usuarios.domain.dto.AutenticacaoDTO;
 import payment_system.usuarios.domain.dto.LoginResponseDTO;
 import payment_system.usuarios.domain.dto.RegisterDTO;
-import payment_system.usuarios.application.services.AutorizacaoService;
 
 @RestController
-@RequestMapping("/auth/user")
+@RequestMapping("api/auth/user")
 public class AutenticacaoController {
 
-    @Autowired
-    private AutorizacaoService autenticacaoService;
-
+    @Autowired private AutorizacaoService autenticacaoService;
 
     @PostMapping("/register")
     public ResponseEntity<?> registrar(@RequestBody RegisterDTO dto) {
